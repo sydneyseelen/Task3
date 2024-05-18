29f002c21c08e539c9047d233cc184b27247e08d
Robin1010101010 <104255745+Robin1010101010@users.noreply.github.com>
2024-05-18 12:50:36 +0200

    Implemented location logging

--- app/src/main/java/com/example/gpsservice/GPSService.java
+++ app/src/main/java/com/example/gpsservice/GPSService.java
@@ -1,17 +1,57 @@
 package com.example.gpsservice;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresPermission;
 
-public class GPSService extends Service {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class GPSService extends Service implements LocationListener {
+    private final String TAG = GPSService.class.getCanonicalName();
+
+    private LocationManager locationManager;
+    long minTime = 1000;
+    int minDistance = 10;
+    double longitude, latitude;
+
     private GPSServiceImpl impl;
 
-    private class GPSServiceImpl extends IGPSService.Stub{
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+
+        Log.i("onLocationChanged", "longitude: " + Double.toString(longitude));
+        Log.i("onLocationChanged", "latitude: " + Double.toString(latitude));
+
+        exportLocationToFile(longitude, latitude);
+
+        //tvLong.setText(Double.toString(longitude));
+        //tvLat.setText(Double.toString(latitude));
+    }
+
+    private class GPSServiceImpl extends IGPSService.Stub {
 
         @Override
         public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
@@ -34,6 +74,7 @@ public class GPSService extends Service {
             return 1.0;
         }
     }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -41,22 +82,71 @@ public class GPSService extends Service {
         return (IBinder) impl;
     }
 
+    @RequiresPermission(ACCESS_FINE_LOCATION)
     @Override
-    public void onCreate(){
+    public void onCreate() {
         Log.i("onCreate", "creating service");
         super.onCreate();
-        impl= new GPSServiceImpl();
+        impl = new GPSServiceImpl();
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, this);
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         Log.i("onDestroy", "destroying service");
         super.onDestroy();
+
+        locationManager.removeUpdates(this);
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId){
+    public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("onStartCommand","starting service ");
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void exportLocationToFile(double longitude, double latitude) {
+        try {
+            String filename = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS) + "/location_history.txt";
+            String entry = "longitude: " + longitude + ", latitude: " + latitude;
+
+            // construct file metadata
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename);
+            values.put(MediaStore.MediaColumns.MIME_TYPE, "text/plain");
+            values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS);
+
+            // request to write the file to the media store
+            ContentResolver resolver = this.getContentResolver();
+            Uri uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values);
+
+            if (uri == null) {
+                Log.i(TAG, "Failed to obtain an uri to write to");
+                return;
+            }
+
+            OutputStream outputStream = resolver.openOutputStream(uri, "wa");
+
+            if (outputStream == null) {
+                Log.i(TAG, "Failed to obtain an output stream to write to");
+                return;
+            }
+
+            Log.i(TAG, "Writing history to media store");
+
+            PrintWriter out = new PrintWriter(outputStream);
+            out.println(entry);
+            out.close();
+
+            //FileOutputStream outputStream = new FileOutputStream(filename, true);
+            //byte[] strToBytes = entry.getBytes();
+            //outputStream.write(strToBytes);
+
+            Log.i(TAG, "History is written to media store");
+        } catch (IOException e) {
+            Log.i(TAG, "Failed to export history:\n" + e);
+        }
     }
 }
--- app/src/main/java/com/example/gpsservice/MainActivity.java
+++ app/src/main/java/com/example/gpsservice/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.gpsservice;
 
+import android.Manifest;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements ServiceConnection {
-    final private String TAG = MainActivity.class.getCanonicalName();
+    private final static String TAG = MainActivity.class.getCanonicalName();
     private IGPSService gpsServiceProxy = null;
+
+    private Button startServiceButton;
+    private TextView tvLong, tvLat;
+    int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +43,44 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        startServiceButton = findViewById(R.id.StartServiceButton);
+        tvLat = findViewById(R.id.latitude);
+        tvLong = findViewById(R.id.longitude);
+
         Intent i = new Intent (this, GPSService.class);
-        Button startServiceButton = findViewById(R.id.StartServiceButton);
-        startServiceButton.setOnClickListener(new View.OnClickListener(){
+
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                if (gpsServiceProxy!=null){
+                startService(i);
+
+                if (gpsServiceProxy!=null) {
                     Log.i("onClick", "starting service");
                     startService(i);
-                }else{
+                } else {
                     Log.i("onClick", "no proxy ");
                 }
             }
         });
 
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        if (!isGPSEnabled(this)) {
+            enableLocationSettings();
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=
+                PackageManager.PERMISSION_GRANTED){
+            requestLocationPermission();
+        }
+    }
+
     @Override
     public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
         Log.i(TAG, "Service connected");
@@ -57,4 +93,29 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         gpsServiceProxy = null;
     }
 
+    public boolean isGPSEnabled (Context mContext){
+        LocationManager locationManager = (LocationManager)
+                mContext.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+    }
+
+    private void enableLocationSettings() {
+        new AlertDialog.Builder(this)
+                .setTitle("enable GPS")
+                .setMessage("GPS currently disabled. Do you want me to enable GPS?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(settingsIntent);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 }
